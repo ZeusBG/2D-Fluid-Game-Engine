21e15e7ff54d519c5856ae3f57214d0b816f9170
ZeusBG <archerbg123@gmail.com>
2017-08-22 15:05:31 +0300

    Added camera component to one of the entites in the level.json
     * Removed some unused code after rebasing master

--- src/game/GameClient.cpp
+++ src/game/GameClient.cpp
@@ -12,17 +12,6 @@ void GameClient::Init(const SystemSettings& settings)
     m_Engine->SetSystemSettings(settings);
     m_Engine->Init(settings);
     m_Engine->LoadMap("resources/levels/SimpleLevel.json");
-
-    simpleEntity->AddComponent(ObjectsFactory::CreateComponent("SimpleVisualComponent"));
-    simpleEntity->AddComponent(ObjectsFactory::CreateComponent("CharacterControllerComponent"));
-    simpleEntity->AddComponent(ObjectsFactory::CreateComponent("CameraComponent"));
-
-
-    std::shared_ptr<Entity> simpleEntity2(new SimpleEntity());
-    simpleEntity2->AddComponent(ObjectsFactory::CreateComponent("SimpleVisualComponent"));
-    m_Engine->AddEntity(simpleEntity2);
-
-    
 }
 
 GameClient::~GameClient()
